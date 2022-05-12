7d1b60853beecc5b91e18c39b4c382e7631f2031
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-05-12 14:30:55 -0300

    Create ex021-Cidade SANTO nome SILVA.py
    Programa em Python que testa se a cidade informada possui Santo no nome, e verifica se a pessoa possui o sobrenome Silva.

---
+++ ex021-Cidade SANTO nome SILVA.py
@@ -0,0 +1,6 @@
+# Exercício 24
+a = (input('Diga o nome de uma cidade: ')).strip()
+print(a[:5].upper() == 'SANTO')
+# Exercício 25
+b = (input('Digite seu nome: ')).strip()
+print('SILVA' in b.upper())
