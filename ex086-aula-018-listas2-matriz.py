0596439de17fe9cedb6bd4f0a5cad26eb41e8a22
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-05-31 09:29:46 -0300

    Create ex086-aula-018-listas2-matriz.py

---
+++ ex086-aula-018-listas2-matriz.py
@@ -0,0 +1,10 @@
+# Exercício 86 Aula 18 - Matriz 3x3
+matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matrix[l][c] = int(input(f'Digite um valor para [{l}] [{c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matrix[l][c]:^5}]', end='')
+    print()
