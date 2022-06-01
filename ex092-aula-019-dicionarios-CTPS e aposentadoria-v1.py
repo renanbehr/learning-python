564e2cb6448eb10af68e5da643e0363d07d716dc
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-06-01 08:39:39 -0300

    Create ex092-aula-019-dicionarios-CTPS e aposentadoria-v1.py

---
+++ ex092-aula-019-dicionarios-CTPS e aposentadoria-v1.py
@@ -0,0 +1,14 @@
+# Exercício 92 Aula 19 - Dicionário que aponta com quantos anos a pessoa aposenta
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('CTPS [0 não tem]: '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação']) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f' - {k} tem o valor {v}')
