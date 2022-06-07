eb06102e8cb28ca1b09bb4aca6ea2ce1ad0bc0c2
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-06-07 14:13:42 -0300

    Create ex096B-aula-020-funções-área de terreno-v1.py

---
+++ ex096B-aula-020-funções-área de terreno-v1.py
@@ -0,0 +1,12 @@
+# Exercício 96 - Aula 20 - Função que calcula área de terreno
+def area(l, c):
+    area = l * c
+    print(f'Um terreno com largura {l} m e comprimento {c} m possui a àrea {area} m2.')
+
+
+# Programa principal
+area(90, 80)
+area(50, 60)
+area(40, 40)
+area(c=20, l=30)
+area(l=20, c=25)
