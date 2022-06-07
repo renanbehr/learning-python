06fd5b279bde7d2712ef6899fa457dea2e70df05
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-06-07 14:14:41 -0300

    Create ex097-aula-020-funções-print especial-v1.py

---
+++ ex097-aula-020-funções-print especial-v1.py
@@ -0,0 +1,11 @@
+# Exercício 97 - Aula 20 - Função print especial RENAN
+def escreva(txt):
+    print('~' * 30)
+    print(txt)
+    print('~' * 30)
+
+
+# Programa principal
+print('P R O G R A M A   P R I N T   E S P E C I A L')
+txt = str(input('Digite o texto que você deseja ver em P R I N T   E S P E C I A L: '))
+escreva(txt)
