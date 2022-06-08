f1be7890c31cb19f65b2d71581437910ec04542b
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-06-08 08:17:03 -0300

    Create ex107A-aula-022-módulos e pacotes-anotações.py

---
+++ ex107A-aula-022-módulos e pacotes-anotações.py
@@ -0,0 +1,9 @@
+# Anotações Aula 22 - Módulos e Pacotes
+# Modularização
+
+
+num = int(input('Digite um número: '))
+fat = fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {dobro(num)}')
+print(f'O triplo de {num} é {triplo(num)}')
