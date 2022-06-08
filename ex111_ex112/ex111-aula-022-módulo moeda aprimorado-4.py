79fc24a869efe8bb8c232ebdd689a34109700218
renanbehr <92093888+renanbehr@users.noreply.github.com>
2022-06-08 08:27:45 -0300

    Add files via upload

---
+++ ex111_ex112/ex111-aula-022-módulo moeda aprimorado-4.py
@@ -0,0 +1,4 @@
+from ex111_ex112.Funcionalidades import moeda
+
+p = float(input('Digite um valor R$ '))
+moeda.resumo(p, 17.5, 3.14)
---
+++ ex111_ex112/ex112-aula-022-módulo moeda aprimorado-4.py
@@ -0,0 +1,5 @@
+from ex111_ex112.Funcionalidades import moeda
+from ex111_ex112.Funcionalidades import dado
+
+p = dado.leiaDinheiro('Digite um valor R$ ')
+moeda.resumo(p, 17.5, 3.14)
